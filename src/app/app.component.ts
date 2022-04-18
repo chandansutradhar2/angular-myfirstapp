a1fcfe069f60d289c181a42c07e985d28550c818
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-18 17:29:40 +0530

    added styling and string interpolation demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'myfirstapp';
+  currentDate!: string;
+
+  constructor() {
+    setInterval(() => {
+      this.currentDate = new Date().toLocaleString();
+    }, 1000);
+  }
+
+  showDateTime() {}
 }
