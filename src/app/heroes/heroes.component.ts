2d14392adabea1a76fa542394f59cd6828b420d3
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-20 16:02:10 +0530

    hero editor work

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../models/hero.model';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
-  a: string = '';
+  // hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstrom',
+    rating: 10,
+  };
   isLoggedIn: boolean = false;
   constructor() {
     setTimeout(() => {
