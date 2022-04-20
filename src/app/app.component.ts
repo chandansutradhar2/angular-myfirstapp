09ef9672671831dfa155755036bf5f7146014a7f
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-20 14:51:41 +0530

    day 3 work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,39 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  currentDate!: string;
-  name: string = 'chandan';
-  constructor() {
-    setInterval(() => {
-      this.currentDate = new Date().toLocaleString();
-    }, 1000);
-  }
+  title: string = 'Hero Angular App';
+  constructor() {}
 
   getCurrentUserName(): string {
     return 'Chandan Naresh';
   }
 
   showDateTime() {}
+
+  fun1() {
+    return new Observable((obs) => {
+      setInterval(() => {
+        console.log('inside fun1');
+        obs.next('new msg recieved');
+      }, 2200);
+    });
+    // return new Promise((resolve, reject) => {
+    //   //let timeValue = Math.floor(Math.random()*1000);
+    //
+    // });
+  }
+
+  caller() {
+    // this.fun1().then((msg) => {
+    //   console.log('new event :', msg);
+    // });
+    this.fun1().subscribe((msg) => {
+      console.log('new event :', msg);
+    });
+  }
 }
