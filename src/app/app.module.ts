0a1b1f2174aeeed0f1f99e97f065d7c1fea9a5f0
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-20 16:41:42 +0530

    adding features module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { DemoComponent } from './demo/demo.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, DemoComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
