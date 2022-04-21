cc7297a67470dc7c230fbaffe4e9201e7b288cc8
chandan_naresh <chandansutradhar2@gmail.com>
2022-04-21 10:46:34 +0530

    disply list and detail suing ngFor and event handling

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mockup/mock-heroes';
 import { Hero } from '../models/hero.model';
 @Component({
   selector: 'app-heroes',
@@ -7,27 +8,37 @@ import { Hero } from '../models/hero.model';
 })
 export class HeroesComponent implements OnInit {
   // hero = 'Windstorm';
+
   hero: Hero = {
     id: 1,
     name: 'Windstrom',
     rating: 10,
   };
-  isLoggedIn: boolean = false;
-  constructor() {
-    setTimeout(() => {
-      this.isLoggedIn = true;
-    }, 7000);
+
+  heroes = HEROES;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.heroes.forEach((hero) => {
+      console.log('', hero.id, hero.name, hero.rating);
+    });
   }
-
-  ngOnInit(): void {}
-
-  // onChange() {
-  //   // let name = document.getElementById('name')?.nodeValue;
-  //   // console.log(name);
-  //   console.log('test');
-  // }
 
   print() {
     console.log(this.hero);
+  }
+
+  onSelect(hero: Hero) {
+    console.log(hero);
+  }
+
+  onChange(ev: any, id: number) {
+    console.log(ev.target.value);
+    let result = this.heroes.findIndex((hero) => {
+      return hero.id == id;
+    });
+
+    result !== -1 ? (this.heroes[result].name = ev.target.value) : null;
   }
 }
---
+++ src/app/mockup/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../models/hero.model';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice', rating: 5 },
+  { id: 12, name: 'Narco', rating: 4 },
+  { id: 13, name: 'Bombasto', rating: 4.4 },
+  { id: 14, name: 'Celeritas', rating: 4.1 },
+  { id: 15, name: 'Magneta', rating: 5 },
+  { id: 16, name: 'RubberMan', rating: 2.5 },
+  { id: 17, name: 'Dynama', rating: 4 },
+  { id: 18, name: 'Dr IQ', rating: 4.2 },
+  { id: 19, name: 'Magma', rating: 3.5 },
+  { id: 20, name: 'Tornado', rating: 5 },
+];
